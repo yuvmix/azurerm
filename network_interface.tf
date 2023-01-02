7eef1ee089ba9840cf75291e9fd6823f7f23d5bf
yuvmix <yuvalam@matrix.co.il>
2023-01-02 02:29:05 -0500

    network_interface.tf adding _ and fixing location

--- network_interface.tf
+++ network_interface.tf
@@ -1,15 +1,15 @@
 resource "azurerm_network_interface" "dev_nic" {
-  name = "dev_nic"
-  location = azurerm_resource_group.dev_rg.locaiton
+  name                = "dev_nic"
+  location            = azurerm_resource_group.dev_rg.location
   resource_gruop_name = azurerm_resource_group.dev_rg.name
-  
+
   ip_configuration {
-    name = "internal"
-    subnet_id = azurerm_subnet.dev_subnet.id
-    private_ip address_allocation = "Static"
-    public_ip_address_id = azurerm_public_ip.dev_ip.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.dev_subnet.id
+    private_ip_address_allocation = "Static"
+    public_ip_address_id          = azurerm_public_ip.dev_ip.id
   }
-  
+
   tags = {
     environment = "dev"
   }
