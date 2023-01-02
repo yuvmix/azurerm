b50509815c9458e082b18f75fd6de0c339e71d3f
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-02 09:42:04 +0200

    Create linux_virtual_machine.tf

---
+++ linux_virtual_machine.tf
@@ -0,0 +1,27 @@
+resource "azurerm_linux_virtual_machine" "dev_lvm" {
+  name = "dev_lvm"
+  resource_group_name = azurerm_resource_group.dev_rg.name
+  location = azurerm_resource_group.dev_rg.location
+  size = "Standard_F2"
+  admin_username = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.dev_nic.id
+    ]
+  
+  admin_ssh_key {
+    username = "adminuser" 
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+  
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  
+  source_image_referance {
+    publisher = "Canonical"
+    offer = "UbuntuServer"
+    sku = "20.04-LTS"
+    version = "latest"
+  }
+}
