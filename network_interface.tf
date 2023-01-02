31c869d1dadc3053047da788624db3ed123fd518
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-02 09:26:30 +0200

    Create network_interface.tf

---
+++ network_interface.tf
@@ -0,0 +1,16 @@
+resource "azurerm_network_interface" "dev_nic" {
+  name = "dev_nic"
+  location = azurerm_resource_group.dev_rg.locaiton
+  resource_gruop_name = azurerm_resource_group.dev_rg.name
+  
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.dev_subnet.id
+    private_ip address_allocation = "Static"
+    public_ip_address_id = azurerm_public_ip.dev_ip.id
+  }
+  
+  tags = {
+    environment = "dev"
+  }
+}
