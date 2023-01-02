c9cf7bf3b54c08db84af88d2e12b10b2f61c58ad
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-02 09:01:22 +0200

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,6 @@
+resource "azurerm_subnet" "dev_subnet" {
+  name = "dev_subnet"
+  resource_group_name = azurerm_resource_group.dev_rg.name
+  virtual_network_name = azurerm_virtual_network.dev_vn.name
+  address_prefixes = ["10.123.1.0/24"]
+}
