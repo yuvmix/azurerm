5b8d3c041b04fbab3d36d03f67bd6f74a7644503
yuvmix <yuvalam@matrix.co.il>
2023-01-02 02:14:13 -0500

    fix to security rule inbound

--- security_role.tf
+++ security_role.tf
@@ -1,13 +1,13 @@
 resource "azurerm_network_security_rule" "dev_sr" {
-  name = "dev_sr"
-  priority = 100
-  direction = "Inbount"
-  access = "Allow"
-  protocol = "*"
-  source_port_range = "*"
-  destination_port_range = "*"
-  source_address_prefix = "*"
-  destination_address_prefix = "*"
-  resource_group_name = azurerm_resource_group.dev_rg.name
+  name                        = "dev_sr"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.dev_rg.name
   network_security_group_name = azurerm_network_security_group.dev_sg.name
 }
