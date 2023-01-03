708021102408bc42e252c4e1f1f66026ff122df0
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-03 15:08:13 +0200

    Update network_interface.tf

--- network_interface.tf
+++ network_interface.tf
@@ -10,7 +10,7 @@ resource "azurerm_network_interface" "dev_nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.dev_subnet.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.123.1.${count.index + 5}"
+    private_ip_address            = "10.123.1.${count.index + 5}" # first 3 and last 2 addrsses reserved by azure(of each subnet)
     public_ip_address_id          = azurerm_public_ip.dev_ip[count.index].id
   }
 
