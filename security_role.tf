104a27bac1b936d8d8006af1903614990638d7db
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-03 14:13:00 +0200

    Update security_role.tf

--- security_role.tf
+++ security_role.tf
@@ -13,4 +13,8 @@ resource "azurerm_network_security_rule" "dev_sr" {
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.dev_rg.name
   network_security_group_name = azurerm_network_security_group.dev_sg.name
+  
+  tags = {
+    environment = "dev"
+  }
 }
