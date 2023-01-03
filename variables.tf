f7e05cff76c1d424da678f3d3fb26079f8452797
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-03 15:51:11 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,53 @@
+# resource_group.tf
+variable "location" {
+  default = "West Europe"
+}
+
+# virtual_network.tf
+variable "vn_address_space" {
+  default = ["10.123.0.0/16"]
+}
+
+# subnet.tf
+variable "address_prefixes" {
+  default = ["10.123.1.0/24"]
+}
+
+# public_ip.tf
+variable "ip_allocation_method" {
+  default = "Static"
+}
+
+# security rule.tf
+variable "rule_protocol_set" {
+  default = toset(["http", "ssh"])
+}
+
+# linux_virtual_machine.tf 
 variable "vms_amount" {
   default = 2
+}
+
+variable "lvm_size" {
+  default = "Standard_F2"
+}
+
+variable "username" {
+  default = "adminuser"
+}
+
+variable "custom_data_file_path" {
+  default = "~/azurerm/scripts/custom_data.tpl"
+}
+
+variable "public_key_path_file" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "templatefile_template_path" {
+  default = "scripts/my_linux_ssh_script.tpl"
+}
+
+variable "identityfile" {
+  default =  "~/.ssh/id_rsa"
 }
