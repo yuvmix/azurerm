fedc29060de9d3a742c6de92c40c9c9f82c3857d
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-03 15:09:23 +0200

    Update public_ip.tf

--- public_ip.tf
+++ public_ip.tf
@@ -1,3 +1,4 @@
+# the publics ips for the vms
 resource "azurerm_public_ip" "dev_ip" {
 
   count = var.vms_amount
@@ -5,7 +6,7 @@ resource "azurerm_public_ip" "dev_ip" {
   name                = "dev_ip_${count.index}"
   resource_group_name = azurerm_resource_group.dev_rg.name
   location            = azurerm_resource_group.dev_rg.location
-  allocation_method   = "Static"
+  allocation_method   = "Static" # or Dynamic
 
   tags = {
     environment = "dev"
