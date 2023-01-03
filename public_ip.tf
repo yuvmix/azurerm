ae9315d0bf5da1fdc1a9c87b9fb45f33985c4e55
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-03 15:25:54 +0200

    Update public_ip.tf

--- public_ip.tf
+++ public_ip.tf
@@ -6,7 +6,7 @@ resource "azurerm_public_ip" "dev_ip" {
   name                = "dev_ip_${count.index}"
   resource_group_name = azurerm_resource_group.dev_rg.name
   location            = azurerm_resource_group.dev_rg.location
-  allocation_method   = "Static" # or Dynamic
+  allocation_method   = var.ip_allocation_method
 
   tags = {
     environment = "dev"
