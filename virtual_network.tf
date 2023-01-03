7bd46acef87a22c2dc62de2d3d944ec621c9a1a0
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-03 15:19:54 +0200

    Update virtual_network.tf

--- virtual_network.tf
+++ virtual_network.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "dev_vn" {
   name                = "dev_vn"
   resource_group_name = azurerm_resource_group.dev_rg.name
   location            = azurerm_resource_group.dev_rg.location
-  address_space       = ["10.123.0.0/16"]
+  address_space       = var.vn_address_space
 
   tags = {
     environment = "dev"
