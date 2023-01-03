166b24d6c85a85c8288f0a74313e4df47b189a79
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-03 15:11:18 +0200

    Update security_group_association.tf

--- security_group_association.tf
+++ security_group_association.tf
@@ -1,3 +1,4 @@
+# associate between subnet to sg
 resource "azurerm_subnet_network_security_group_association" "dev_sga" {
   subnet_id                 = azurerm_subnet.dev_subnet.id
   network_security_group_id = azurerm_network_security_group.dev_sg.id
