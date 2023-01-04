1ebbec28b88ec8ca81209d8e2cf2121588f353a7
yuvmix <113332005+yuvmix@users.noreply.github.com>
2023-01-04 08:52:50 +0200

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -2,8 +2,8 @@ resource "azurerm_public_ip" "dev_lb_ip" {
   name                = "dev_lb_ip"
   location            = azurerm_resource_group.dev_rg.location
   resource_group_name = azurerm_resource_group.dev_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard" # has to be as lb sku
+  allocation_method   = var.lb_ip_allocation_method
+  sku                 = var.lb_sku
   
   tags = {
     environment = "dev"
@@ -14,11 +14,11 @@ resource "azurerm_lb" "dev_lb" {
   name                = "dev-lb"
   location            = azurerm_resource_group.dev_rg.location
   resource_group_name = azurerm_resource_group.dev_rg.name
-  sku                 = "Standard" # has to be as public ip sku
+  sku                 = var.lb_sku
 
   # the configuration of the recieved ip of the lb
   frontend_ip_configuration {
-    name                 = "dev_lb_public_ip"
+    name                 = azurerm_public_ip.dev_lb_ip.name
     public_ip_address_id = azurerm_public_ip.dev_lb_ip.id
   }
   
@@ -52,11 +52,11 @@ resource "azurerm_lb_backend_address_pool_address" "dev_lb_address_pool_address"
   }
 }
 
-# health probe to check the addressed in the pool
-resource "azurerm_lb_probe" "dev_lb_http_probe" {
+# health probe to check the addresses in the pool
+resource "azurerm_lb_probe" "dev_lb_${var.lb_rule_name}_probe" {
   loadbalancer_id = azurerm_lb.dev_lb.id
-  name            = "dev_lb_http_probe"
-  port            = 80
+  name            = "dev_lb_${var.lb_rule_name}_probe"
+  port            = var.lb_rule_backend_port
   protocol        = "Tcp"
   
   tags = {
@@ -65,13 +65,13 @@ resource "azurerm_lb_probe" "dev_lb_http_probe" {
 }
 
 # rule to address conectivity to the pool from the frontend ip of the lb
-resource "azurerm_lb_rule" "dev_lb_http_rule" {
+resource "azurerm_lb_rule" "dev_lb_${var.lb_rule_name}_rule" {
   loadbalancer_id                = azurerm_lb.dev_lb.id
-  name                           = "dev_lb_http_rule"
+  name                           = "dev_lb_${var.lb_rule_name}_rule"
   protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "dev_lb_public_ip"
+  frontend_port                  = var.lb_rule_frontend_port
+  backend_port                   = var.lb_rule_backend_port
+  frontend_ip_configuration_name = azurerm_public_ip.dev_lb_ip.name
   probe_id                       = azurerm_lb_probe.dev_lb_http_probe.id # should attach the probe here so the rule will apply it
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.dev_lb_address_pool.id] # which pools to activate the rule on
   
